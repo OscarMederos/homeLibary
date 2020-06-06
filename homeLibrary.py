ddbb0cae141d4c8c030af712777e53c1173e025c
osmed086 <OscarMederos@users.noreply.github.com>
2020-06-06 09:16:38 -0400

    Update homeLibrary.py

--- homeLibrary.py
+++ homeLibrary.py
@@ -9,6 +9,15 @@ cur.execute("CREATE TABLE ds(name TEXT PRIMARY KEY, publisher TEXT)")
 cur.execute("CREATE TABLE ps3(name TEXT PRIMARY KEY, publisher TEXT)")
 cur.execute("CREATE TABLE switch(name TEXT PRIMARY KEY, publisher TEXT)")
 cur.execute("CREATE TABLE \"3ds\"(name TEXT PRIMARY KEY, publisher TEXT)")
+cur.execute("CREATE TABLE gba(name TEXT PRIMARY KEY, publisher TEXT)")
+cur.execute("CREATE TABLE ps4(name TEXT PRIMARY KEY, publisher TEXT)")
+cur.execute("CREATE TABLE wii(name TEXT PRIMARY KEY, publisher TEXT)")
+cur.execute("CREATE TABLE xboxone(name TEXT PRIMARY KEY, publisher TEXT)")
+cur.execute("CREATE TABLE ps1(name TEXT PRIMARY KEY, publisher TEXT)")
+cur.execute("CREATE TABLE xbox360(name TEXT PRIMARY KEY, publisher TEXT)")
+cur.execute("CREATE TABLE xbox(name TEXT PRIMARY KEY, publisher TEXT)")
+cur.execute("CREATE TABLE wiiu(name TEXT PRIMARY KEY, publisher TEXT)")
+
 
 # begin read psp csv file into dictionary & insert into matching table
 with open('psp.csv', 'r') as fin:
@@ -60,8 +69,18 @@ with open('3ds.csv', 'r') as fin:
 cur.executemany("INSERT INTO \"3ds\"(name, publisher) VALUES(?, ?)", to_db)
 con.commit()
 
+# begin read gba csv file into dictionary & insert into matching table
+with open('gba.csv', 'r') as fin:
+    dr = csv.reader(fin)
+    to_db = []
+    for line in dr:
+        to_db.append(line)
+
+cur.executemany("INSERT INTO gba(name, publisher) VALUES(?, ?)", to_db)
+con.commit()
+
 # output the contents of a table to verify integrity
-cur.execute("SELECT name, publisher FROM \"3ds\"")
+cur.execute("SELECT name, publisher FROM gba")
 rows = cur.fetchall()
 for row in rows:
     print(row)
